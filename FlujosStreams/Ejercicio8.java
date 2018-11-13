f70d97bb0ea75a6b56998787255b2031f3d29390
yeraynotario <yeraynotario@iesjoandaustria.org>
2018-11-13 20:42:53 +0100

    Añado archivo

--- FlujosStreams/Ejercicio8.java
+++ FlujosStreams/Ejercicio8.java
@@ -11,9 +11,17 @@ public class Ejercicio8 {
 	public static void main (String [] args) {
 		try {
 			BufferedWriter fichero = new BufferedWriter (new FileWriter("newtext.txt")); // Atención: utilizamos otro constructor de FileReader. Enestá ocasión no utilizamos un objeto File sino le pasamos el nombre del archivo
-			String linea;
-			while ((linea = fichero.newLine()) != null) // leemos de línea en línea
-			System.out.println (linea);
+			
+			String fila [] = new String [10];
+			int contador=0;
+			
+			for(int i=0; i<fila.length; i++) {
+				contador++;
+				fichero.write("Esta es la fila "+contador);
+				fichero.newLine();
+				
+			}
+			
 			fichero.close();
 		}
 		catch (FileNotFoundException fn) {
