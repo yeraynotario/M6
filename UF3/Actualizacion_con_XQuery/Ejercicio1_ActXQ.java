2f258615f3d97966449db123b7270d053b426ae3
Yeray Notario <yeraynotario@iesjoandaustria.org>
2019-03-26 20:57:10 +0100

    Add files via upload

---
+++ UF3/Actualizacion_con_XQuery/Ejercicio1_ActXQ.java
@@ -0,0 +1,35 @@
+import javax.xml.xquery.XQConnection;
+import javax.xml.xquery.XQDataSource;
+import javax.xml.xquery.XQException;
+import javax.xml.xquery.XQExpression;
+import net.xqj.exist.ExistXQDataSource;
+
+public class Ejercicio1_ActXQ {
+	public static void main(String[] args) {
+		try {
+			XQDataSource server = new ExistXQDataSource();
+			server.setProperty ("serverName", "192.168.56.102");
+			server.setProperty ("port","8080");
+			server.setProperty ("user","admin");
+			server.setProperty ("password","austria");
+			XQConnection conn = server.getConnection();
+			//XQPreparedExpression consulta;
+
+			//consulta = conn.createExpression();
+			XQExpression consulta = conn.createExpression();
+			String actual = ("update value " +"/EMPLEADOS/EMP_ROW[EMP_NO=7369]/APELLIDO " +"with 'NOTARIO'");
+		    //consulta.executeCommand("removeDocument($docId)");
+			
+			
+			consulta.executeCommand(actual);
+			
+			
+			System.out.println("Se ha modificado el usuario!");
+		    consulta.close();
+			
+		} catch (XQException e) {
+			e.printStackTrace();
+			System.out.println("Error!");
+		} 
+	}
+}
